9feea01b45cd4740d57e80454a5c902ea53a86cd
Steven <stevenzhaooooo@gmail.com>
2019-07-30 15:05:32 +1200

    -bug fixed crashes, -UI completely working

--- HereAfter/Source/HereAfter/MyGameManager.cpp
+++ HereAfter/Source/HereAfter/MyGameManager.cpp
@@ -23,13 +23,13 @@ void AMyGameManager::BeginPlay()
 	Super::BeginPlay();
 
 	QuestMan = GetWorld()->SpawnActor<AQuestManager>(AQuestManager::StaticClass());
-	QuestMan->AddQuest("BoxQuest", "Unearth casket", false, false, false);
-	QuestMan->AddQuest("DeerQuest", "Find doe", false, false, false);
-	QuestMan->AddQuest("PlantQuest", "Retrieve a Mandragora", false, false, false);
-	QuestMan->AddQuest("WaterQuest", "Collect a vial of well water", true, false, false);
-
+	
 
 	
+	QuestMan->AddQuest("WaterQuest", "Collect a vial of well water", false, false, false);
+	QuestMan->AddQuest("PlantQuest", "Retrieve a Mandragora", false, false, false);
+	QuestMan->AddQuest("DeerQuest", "Find doe", false, false, false);
+	QuestMan->AddQuest("BoxQuest", "Unearth casket", false, false, false);
 	//QuestMan->AddQuest("Get Shyamalaned", "Traverse the woods to find the twist", false, false, false); 
 	/*QuestMan->AddQuest("Get Water", "Traverse the woods to find the water", true, false, false);
 	QuestMan->AddQuest("Get Dirt", "Traverse the woods to find the dirt", false, false, false);
